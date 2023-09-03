33eaaa3262bb8735716904d7a159e8ca79d09eb8
Daniel Campos <giusniyyel@gmail.com>
2023-09-03 17:38:04 -0600

    initial commit

---
+++ src/main/java/com/giusniyyel/ctrctfrstwebshop/CtrctFrstWebshopApplication.java
@@ -0,0 +1,13 @@
+package com.giusniyyel.ctrctfrstwebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtrctFrstWebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtrctFrstWebshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giusniyyel/ctrctfrstwebshop/CtrctFrstWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giusniyyel.ctrctfrstwebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtrctFrstWebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
